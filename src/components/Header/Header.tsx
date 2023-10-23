ad77326c6b33484bce0041f15f2048adf3fdf4fa
vinicius webster <viniciuswebster99@gmail.com>
2023-10-23 11:55:16 -0300

    style: responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { mdColor, veryDarkColor } from "../../UI/variables";
 import { BsCodeSlash } from "react-icons/bs";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import { HashLink } from "react-router-hash-link";
 
 const StyledHeader = styled.header`
   display: flex;
@@ -17,21 +18,38 @@ const StyledHeader = styled.header`
       margin-right: 0;
     }
   }
+
+  @media screen and (max-width: 500px){
+    padding: 1rem;
+    div > a {
+    margin-right: 12px;
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+  }
 `;
 
 const StyledNav = styled.nav`
   display: flex;
   align-items: center;
   gap: 32px;
+  
+  @media screen and (max-width: 500px){
+   gap: 16px; 
+  }  
 `;
 
 const StyledUl = styled.ul`
   display: flex;
   align-items: center;
   gap: 24px;
+  @media screen and (max-width: 500px){
+   gap: 16px; 
+  }  
 `;
 
-const StyledA = styled.a`
+const StyledA = styled(HashLink)`
   color: ${mdColor};
   font-weight: 600;
 `;
@@ -40,21 +58,31 @@ const Header = () => {
   return (
     <StyledHeader>
       <StyledNav>
-        <BsCodeSlash color={mdColor} size={40} />
+        <HashLink to={"/"}>
+          <BsCodeSlash color={mdColor} size={40} />
+        </HashLink>
         <StyledUl>
           <li>
-            <StyledA href="#aboutme">Sobre mim</StyledA>
+            <StyledA to="/#aboutme">Sobre mim</StyledA>
           </li>
           <li>
-            <StyledA href="#projects">Projetos</StyledA>
+            <StyledA to="/#projects">Projetos</StyledA>
           </li>
         </StyledUl>
       </StyledNav>
       <div>
-        <a href="https://www.linkedin.com/in/viniwebster/" target="_blank" rel="noreferrer">
+        <a
+          href="https://www.linkedin.com/in/viniwebster/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <AiFillLinkedin color={mdColor} size={40} />
         </a>
-        <a href="https://github.com/viniwebster" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/viniwebster"
+          target="_blank"
+          rel="noreferrer"
+        >
           <AiFillGithub color={mdColor} size={40} />
         </a>
       </div>
